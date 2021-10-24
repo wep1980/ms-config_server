5e237f83af0478d5cf52d81670fbababa8e15e3c
wep1980 <wepbike@gmail.com>
2021-10-24 07:09:49 -0300

    Configuracao do server com github e teste no postman

--- src/main/java/br/com/wepdev/configserver/ConfigServerApplication.java
+++ src/main/java/br/com/wepdev/configserver/ConfigServerApplication.java
@@ -2,7 +2,12 @@ package br.com.wepdev.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Projeto que recebe algumas configurações de todos os micro serviços (configuração centralizada)
+ */
+@EnableConfigServer // Habilitando a configuracao de servidor no projeto
 @SpringBootApplication
 public class ConfigServerApplication {
 
